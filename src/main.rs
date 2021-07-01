5328ad19acab082bf7d45e951e54a49e424bcd13
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2021-07-01 13:26:42 +1000

    Switch to using the published version of stivale
    * Switch to using the crates.io version of stivale-rs
    (https://github.com/Andy-Python-Programmer/stivale). The crate has been
    published as `stivale-boot`
    * The crate has been fully rewritten and now supports all of the
    structures in the stivale specification so, also includes changes to
    that
    
    Signed By: Anhad Singh

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@
 #![feature(llvm_asm)]
 
 use core::panic::PanicInfo;
-use stivale::{HeaderFramebufferTag, StivaleHeader};
+use stivale_boot::v2::{StivaleFramebufferHeaderTag, StivaleHeader};
 
 static STACK: [u8; 4096] = [0; 4096];
 
-static FRAMEBUFFER_TAG: HeaderFramebufferTag = HeaderFramebufferTag::new().bpp(24);
+static FRAMEBUFFER_TAG: StivaleFramebufferHeaderTag =
+    StivaleFramebufferHeaderTag::new().framebuffer_bpp(24);
 
 #[link_section = ".stivale2hdr"]
 #[no_mangle]
 #[used]
-static STIVALE_HDR: StivaleHeader = StivaleHeader::new(&STACK[4095] as *const u8)
-    .tags((&FRAMEBUFFER_TAG as *const HeaderFramebufferTag).cast());
+static STIVALE_HDR: StivaleHeader = StivaleHeader::new()
+    .stack(&STACK[4095] as *const u8)
+    .tags((&FRAMEBUFFER_TAG as *const StivaleFramebufferHeaderTag).cast());
 
 #[no_mangle]
 extern "C" fn entry_point(_header_addr: usize) -> ! {
